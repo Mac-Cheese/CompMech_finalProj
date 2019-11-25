4827731f32a175ceeccfe86fb2508bdff6685a20
Eric <eric.kostoss@uconn.edu>
2019-11-25 12:14:20 -0500

    Gauss-methods
    I did this for some reason. I think all that is is lets you input an augmented matrix while retaining the other, input both functionality. Lots of conditional statements.

--- GaussNaive.m
+++ GaussNaive.m
@@ -1,4 +1,4 @@
-function x = GaussNaive(A,b)
+function x = GaussNaive(A,varargin)
 % GaussNaive: naive Gauss elimination
 %   x = GaussNaive(A,b): Gauss elimination without pivoting.
 % input:
@@ -6,10 +6,46 @@ function x = GaussNaive(A,b)
 %   b = right hand side vector
 % output:
 %   x = solution vector
-[m,n] = size(A);
-if m~=n, error('Matrix A must be square'); end
-nb = n+1;
-Aug = [A b];
+ipGN=inputParser;
+addRequired(ipGN,'A'); addOptional(ipGN,'b','b');
+parse(ipGN,A,varargin{:})
+assignAb=0; assignb=0;
+[m,n] = size(ipGN.Results.A); [o,p]=size(ipGN.Results.b);
+if m~=n
+    if n==(m+1)
+    disp('Assuming Augmented'); Aug=A; assignAb=1;
+    else
+        error('A must be square')
+    end
+else
+    A=ipGN.Results.A;
+end
+if ~assignAb&&~ischar(ipGN.Results.b)
+    if (p>1)
+        disp('ignoring extraneous b columns'); b=ipGN.Results.b; assignb=1;
+    end
+    if (o>m)
+        if assignb
+            fprintf('Taking first %d rows of b',round(m)); b=b(1:m,1);
+        else
+            fprintf('Taking first %d rows of b',round(m));
+            b=ipGN.Results.b(1:m,1); assignb=1;
+        end
+    end
+    if o<m
+        error('b not sufficiently supplied')
+    elseif ~assignb
+        b=ipGN.Results.b;
+    end
+    nb = n+1;
+    Aug = [A b];
+elseif ~assignAb
+    error('b not sufficiently supplied')
+elseif assignAb&&(ipGN.Results.b(1)~='b')
+    disp('Extraneous b supplied; ignoring'); nb=n; n=n-1;
+elseif assignAb
+    nb=n; n=n-1;
+end
 % forward elimination
 for k = 1:n-1
   for i = k+1:n
--- GaussPivot.m
+++ GaussPivot.m
@@ -1,4 +1,4 @@
-function x = GaussPivot(A,b)
+function x = GaussPivot(A,varargin)
 % GaussPivot: Gauss elimination pivoting
 %   x = GaussPivot(A,b): Gauss elimination with pivoting.
 % input:
@@ -6,25 +6,62 @@ function x = GaussPivot(A,b)
 %   b = right hand side vector
 % output:
 %   x = solution vector
-[m,n]=size(A);
-if m~=n, error('Matrix A must be square'); end
-nb=n+1;
+ipGP=inputParser;
+addRequired(ipGP,'A'); addOptional(ipGP,'b','b');
+parse(ipGP,A,varargin{:})
+assignAb=0; assignb=0;
+[m,n] = size(ipGP.Results.A); [o,p]=size(ipGP.Results.b);
+if m~=n
+    if n==(m+1)
+    disp('Assuming Augmented'); Aug=A; assignAb=1;
+    else
+        error('A must be square')
+    end
+else
+    A=ipGP.Results.A;
+end
+if ~assignAb&&~ischar(ipGP.Results.b)
+    if (p>1)
+        disp('ignoring extraneous b columns'); b=ipGP.Results.b; assignb=1;
+    end
+    if (o>m)
+        if assignb
+            fprintf('Taking first %d rows of b',round(m)); b=b(1:m,1);
+        else
+            fprintf('Taking first %d rows of b',round(m));
+            b=ipGP.Results.b(1:m,1); assignb=1;
+        end
+    end
+    if o<m
+        error('b not sufficiently supplied')
+    elseif ~assignb
+        b=ipGP.Results.b;
+    end
+    nb = n+1;
+    Aug = [A b];
+elseif ~assignAb
+    error('b not sufficiently supplied')
+elseif assignAb&&(ipGP.Results.b(1)~='b')
+    disp('Extraneous b supplied; ignoring'); nb=n; n=n-1;
+elseif assignAb
+    nb=n; n=n-1;
+end
 % forward elimination
 for k = 1:n-1
   % partial pivoting
-  [big,i]=max(abs(A(k:n,k)));
+  [big,i]=max(abs(Aug(k:n,k)));
   ipr=i+k-1;
   if ipr~=k
-    A([k,ipr],:)=A([ipr,k],:);
+    Aug([k,ipr],:)=Aug([ipr,k],:);
   end
   for i = k+1:n
-    factor=A(i,k)/A(k,k);
-    A(i,k:nb)=A(i,k:nb)-factor*A(k,k:nb);
+    factor=Aug(i,k)/Aug(k,k);
+    Aug(i,k:nb)=Aug(i,k:nb)-factor*Aug(k,k:nb);
   end
 end
 % back substitution
 x=zeros(n,1);
-x(n)=A(n,nb)/A(n,n);
+x(n)=Aug(n,nb)/Aug(n,n);
 for i = n-1:-1:1
-  x(i)=(A(i,nb)-A(i,i+1:n)*x(i+1:n))/A(i,i);
+  x(i)=(Aug(i,nb)-Aug(i,i+1:n)*x(i+1:n))/Aug(i,i);
 end
