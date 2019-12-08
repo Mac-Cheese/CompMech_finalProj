c8d2dcc1d6be637290cf1f4b791147c1d917d943
Eric <eric.kostoss@uconn.edu>
2019-12-07 21:05:45 -0500

    progressPublish
    holding my work. If you need to fork from a previous commit, that's fine.

--- finalProject_8.m
+++ finalProject_8.m
@@ -11,16 +11,23 @@ nu=1.5e-5; rho=1.2;%[m^2/s]; [kg/m^3] with laminar valued Reynolds Number
 % implicit solution given as
 eta=@(f) log(sqrt(1+sqrt(f)+f)./(1-sqrt(f)))+...
     sqrt(3)*atan(sqrt(3.*f)./(2+sqrt(f)));
-f=[linspace(0,.9999,3334)];
+f=[linspace(0,.9999,3334)]';
 etaf=eta(f);
 figure(1); plot(etaf,f); xlim([0,10]); xlabel('eta'); ylabel('script f')
 %% I.b
 % derivative
 detaf=diffc2(etaf(2:end),.0003); df=1./detaf;
-figure(2); plot(etaf(2:end),df); xlim([0,10]);
+figure(2); hold off; plot(etaf(2:end),df); xlim([0,10]);
 xlabel('eta'); ylabel('derivative script f')
 %% I.c
 % derivative and proving the thing
 dtf=diffc2(df,.0003);
-figure(3); plot(etaf(2:end),dtf); xlim([0,10]);
+figure(3); plot(etaf(2:end),dtf); xlim([0,10]);%may dump first 2 and last 2
 xlabel('eta'); ylabel('second derivative script f')
+%the thing about shear and skin friction
+%% I.d
+% maximum velocity
+[high,indx]=max(df);
+ply=polyfit(etaf((indx-5):(indx+5)),df((indx-5):(indx+5)),2);
+figure(2); hold on;
+plot(etaf((indx-5):(indx+5)),polyval(ply,etaf((indx-5):(indx+5))))
