bbf3fcc03eedd5081c3ca53561eff06a9f109bbe
Eric <eric.kostoss@uconn.edu>
2019-12-11 11:16:03 -0500

    'cleaning'
    Calls to `n` are now calls to `eta` for readability. `bisect()` changed to `bisectE()` to fix all the little things that have to be changed using the original. `f`, the `bisectE()` output is preallocated, and we don't need `fx`. Calls to `xlim()` are not needed as the new domain `eta` is fixed by the zero-finding method rather than `linspace()` in f. Plot `hold ` calls are moved to before the first plot, so that `hold ` may be simply left `on` after the first plot, wil no need for calls to `clf`.

--- finalProject_8.m
+++ finalProject_8.m
@@ -9,26 +9,24 @@ nu=1.5e-5; rho=1.2;%[m^2/s]; [kg/m^3] with laminar valued Reynolds Number
 %for eta=0, f=df/d'eta=0;  for eta=H=10, df/d'eta=d2f/d'eta^2 = 0
 %% I.a
 % implicit solution given as
-func=@(f,n) log(sqrt(1+sqrt(f)+f)./(1-sqrt(f)))+sqrt(3)*atan(sqrt(3.*f)./(2+sqrt(f)))-n;
-n=0:0.05:10;
-for i=1:length(n)
-    [root(i),fx(i)]=bisectE(@(f) func(f,n(i)),0,1);
+etaf=@(f,n) log(sqrt(1+sqrt(f)+f)./(1-sqrt(f)))...
+    +sqrt(3)*atan(sqrt(3.*f)./(2+sqrt(f)))-n;
+eta=[0:0.05:10]'; f=zeros((10/.05)+1,1);
+for i=1:length(eta)
+    f(i)=bisectE(@(f) etaf(f,eta(i)),0,1, 1e-8);
 end
-f=root';
-n=n';
-f(1)=0;
-figure(1); plot(n,f); xlim([0,10]); xlabel('eta'); ylabel('script f')
+figure(1); hold off; plot(eta,f); xlabel('eta'); ylabel('script f')
 
 %% I.b
 % derivative
 df=diffc2(f,0.05);
-figure(2); plot(n,df); xlim([0,10]);
+figure(2); hold off; plot(eta,df);
 xlabel('eta'); ylabel('derivative script f')
 
 %% I.c
 % derivative and proving the thing
 d2f=diffc2(df,0.05);
-figure(3); plot(n,d2f); xlim([0,10]);
+figure(3); plot(eta,d2f);
 xlabel('eta'); ylabel('second derivative script f')
 fprintf('f"(0): %5.4f\n',d2f(1))
 fprintf('Cf: %5.4f\n',1.778/8)
@@ -36,15 +34,15 @@ fprintf('Cf: %5.4f\n',1.778/8)
 %% I.d
 % maximum velocity
 [fx,index]=goldmin_array(df');
-fprintf('n: %5.4f\n',n(index))
+fprintf('n: %5.4f\n',eta(index))
 fprintf("f'(n): %5.4f\n",df(index))
-ply=polyfit(n((index-5):(index+5)),df((index-5):(index+5)),2);
+ply=polyfit(eta((index-5):(index+5)),df((index-5):(index+5)),2);
 figure(2); hold on;
-plot(n((index-5):(index+5)),polyval(ply,n((index-5):(index+5))));hold off;
+plot(eta((index-5):(index+5)),polyval(ply,eta((index-5):(index+5))));
 
 %% I.e
 % wall jet momentum flu
-Z=trapz(n,df.^2)*trapz(n,df);
+Z=trapz(eta,df.^2)*trapz(eta,df);
 fprintf('Analytical: %5.4f\n',Z)
 fprintf('Theoretical: %5.4f\n',128/(9*4^3))
 
@@ -53,21 +51,20 @@ fprintf('Theoretical: %5.4f\n',128/(9*4^3))
 dfs=df-0.01;
 for i=1:length(df)-1
     if sign(dfs(i))~=sign(dfs(i+1))
-        fprintf('Eta: %6.4f\n',n(i))
+        fprintf('Eta: %6.4f\n',eta(i))
     end
 end
 
 %% I.g
 % Velocity at eta=h
-v_h=3*n(201)*df(201)-f(201);
+v_h=3*eta(201)*df(201)-f(201);
 fprintf('Analytical: %5.4f\n',Z)
 
 %% I.h
 % Solve Eq.(1) and compare to f(n)
 dn=@(x,y) [y(2);y(3);-y(1).*y(3)-2.*y(2).^2];
 [x,fn]=ode45(dn,[0 10],[0 0 Z]);
-figure(1);hold on;
-plot(x,fn(:,1),'y--');hold off;
+figure(1); hold on; plot(x,fn(:,1),'y--');
 
 %% I.i
 % Solve ODE
