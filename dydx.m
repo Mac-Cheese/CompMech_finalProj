f09df5b19341ba91e831a6de7490363dbea21f87
Eric <eric.kostoss@uconn.edu>
2019-12-11 20:03:41 -0500

    dydx.m
    This thing is more accurately d[THETA]deta because that's what it is. It is a second-order theta by eta equation decomposed, and the function was written to take the ouput of I.h

--- dydx.m
+++ dydx.m
@@ -1,3 +1,3 @@
 function dy=dydx(n,y,fn,x)
-fnx=spline(x,fn(:,1),n);
-dy=[y(2);-0.7*fnx*y(2)];
+fnx=spline(x,fn(:,1),n);%tailored to take the ODE ouput of f and f(eta)
+dy=[y(2);-0.7*fnx*y(2)];%solving for dTHETA and dtTHETA from eq(4)
